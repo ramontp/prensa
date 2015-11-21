f025f5d8d753be271148edba30786174a1f8a5c7
Ramón Torres de Paz <ramontp@gmail.com>
2015-11-21 18:12:46 +0100

    script actualización

--- sql/4.2.1/update.sql
+++ sql/4.2.1/update.sql
@@ -1,5 +1,5 @@
 # alter script from previous version.
 
-alter table contactos add email2 varchar(80);
+alter table contactos add observaciones varchar(255);
 alter table contactos add destacado bit(1) default 0;
 alter table contactos add fotografo bit(1) default 0;
